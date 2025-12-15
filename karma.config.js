656e75ad78ea450bbcc1297b3a8e5b17cdc43bd8
SebaCortesP <seba.cortesp@duocuc.cl>
2025-12-15 18:54:45 -0300

    ff

--- karma.config.js
+++ karma.config.js
@@ -1,38 +1,39 @@
 const path = require('node:path');
 
-module.exports = function(config){
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        plugins:[    
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine.html-reporter'),
-            require('karma-coverage'),
-            require('@angular-devkit/build-angular/plugins/karma')
-        ],
-        client: {
-            jasmine:{},
-            clearContent: false
-        },
-        jasmineHtmlReporter:{
-            suppressAll: true
-        },
-        coverageReporter:{
-            dir: path.join(__dirname, './coverage'),
-            subdir: '.',
-            reporters: [
-                { type: 'html', subdir: 'html-report' },
-                { type: 'lcov', subdir: 'lcov-report' }
-            ]
-        },
-        reporters: ['progress', 'kjhtml'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['Chrome'],
-        singleRun: false,
-        restartOnFileChange: true
-    });
-}
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine.html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContent: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html', subdir: 'html-report' },
+        { type: 'lcovonly', file: 'lcov-report' }, // Genera informe HTML
+        { type: 'text-summary' },
+      ],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['Chrome'],
+    autoWatch: false,
+    singleRun: true,
+    restartOnFileChange: true,
+  });
+};
